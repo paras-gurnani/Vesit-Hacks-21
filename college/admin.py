28823920917dc945edfdae75bc4ad811077be387
Paras Gurnani <parasgurnani3@gmail.com>
2021-04-02 23:45:13 +0530

    Changed models of staff and student with user and fixed media storage directories of these models

--- college/admin.py
+++ college/admin.py
@@ -2,17 +2,10 @@ from django.contrib import admin
 
 from .models import Staff, Department, Student, Place, Committee
 # Register your models here.
-<<<<<<< HEAD
 
-admin.site.register(Department)
-admin.site.register(Staff)
-admin.site.register(Student)
-admin.site.register(Place)
-admin.site.register(Committee)
-=======
 admin.site.register(Staff)
 admin.site.register(Department)
 admin.site.register(Student)
 admin.site.register(Place)
 admin.site.register(Committee)
->>>>>>> 734480862532b7d3629fcf51fb7fc506850ed974
+
