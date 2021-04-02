572fe48f39365ad2db167855dabb4e53a50958ca
Paras Gurnani <parasgurnani3@gmail.com>
2021-04-03 00:30:46 +0530

    Added Foreign keys in events model

--- events/models.py
+++ events/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from college import models as mod
 # Create your models here.
 class Event(models.Model):
+
     event_title = models.CharField(max_length = 50, default="")
     event_description = models.CharField(max_length = 100)
     # event_start
@@ -16,4 +17,16 @@ class Event(models.Model):
     registration_link = models.CharField(max_length = 100)
     # postor
     postor = models.ImageField(upload_to = 'Events/Postors', default="")
+    #Foreign Keys
+    event_place = models.ForeignKey(mod.Place, on_delete=models.CASCADE)
+    dept_id = models.ForeignKey(mod.Department,on_delete=models.CASCADE)
+    approval_id  = models.ForeignKey(mod.Staff,on_delete=models.CASCADE)
+    #setting conductor id
+    conductor_id=None
+    if(is_student):
+        conductor_id = models.ForeignKey(mod.Student,on_delete=models.CASCADE)
+    else:
+        conductor_id = models.ForeignKey(mod.Staff,on_delete=models.CASCADE)
+
+
 
