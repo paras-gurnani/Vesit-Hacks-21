1f72af00b4fd9a82bba23e059ddbacdd48b47595
Paras Gurnani <parasgurnani3@gmail.com>
2021-04-03 00:36:21 +0530

    Added dunder methods for super-admin convenience

--- college/models.py
+++ college/models.py
@@ -17,12 +17,19 @@ class Staff(models.Model):
     Staff_type = models.IntegerField()
     gender = models.TextField(max_length=1,default='')
 
+    def __str__(self):
+        return self.user.username
+
 
 class Department(models.Model):
     dept_id = models.AutoField(primary_key=True)
     dept_name = models.CharField(max_length=25)
     dep_hod = models.ForeignKey(Staff,on_delete=models.CASCADE)
-    print(dep_hod)
+    # print(dep_hod)
+
+    def __str__(self):
+        return self.dept_name
+
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -36,15 +43,24 @@ class Student(models.Model):
     stud_photo = models.ImageField(upload_to='college/student/')
     gender = models.CharField(max_length=1,default='')
 
+    def __str__(self):
+        return self.user.username
+
 class Place(models.Model):
     place_id = models.AutoField(primary_key=True)
     place_name = models.CharField(max_length=50)
     capactiy = models.IntegerField()
+
+    def __str__(self):
+        return self.place_name
 
 class Committee(models.Model):
     committee_id = models.AutoField(primary_key=True)
     comm_name = models.CharField(max_length=50)
     comm_level = models.CharField(max_length=30)
     comm_head = models.ForeignKey(Student,related_name='student',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comm_name
 
 
