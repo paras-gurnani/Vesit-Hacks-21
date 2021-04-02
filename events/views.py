3d7ea011ea2e2748f502b03ed9ee80efcb2805aa
Divesh Hariani <diveshharyani92@gmail.com>
2021-04-02 17:07:05 +0530

    Added events title

--- events/views.py
+++ events/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Events page')
+
+def addEvent(request):
+    return HttpResponse('Add Event')
