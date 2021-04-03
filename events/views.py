5a61a24a9c307138e72b2e6a451297c1f448ae92
Divesh Hariani <diveshharyani92@gmail.com>
2021-04-03 11:57:33 +0530

    db changes

--- events/views.py
+++ events/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Event
 from college.models import *
+from datetime import datetime
 
 
 # Create your views here.
@@ -22,28 +23,35 @@ def addEvent(request):
 def storeEvent(request):
     # print(request.POST)
     if(request.method == 'POST'):
-        event = Event()
-        event.event_title = request.POST['title']
-        event.event_description = request.POST['description']
-        event.event_time = request.POST['start_time']
-        event.event_date = request.POST['start_date']
-        event.end_time = request.POST['end_time']
-        event.end_date = request.POST['end_date']
-        event.is_student=True
-        is_student = request.POST['organizer']
-        if(is_student == 1):
-            event.is_student=False
-        event.postor = request.FILES['filename']
-        event.registration_link = request.POST['registration']
+        print(request.POST)
+        title = request.POST['title']
+        description = request.POST['description']
+        start_date = datetime.strptime(request.POST['start_date'], '%Y-%m-%d')
+        start_time = datetime.strptime(request.POST['start_time'], '%I:%M')
+        print(start_date, type(start_date))
+        print(start_time)
+        # event = Event()
+        # event.event_title = request.POST['title']
+        # event.event_description = request.POST['description']
+        # event.event_time = request.POST['start_time']
+        # event.event_date = request.POST['start_date']
+        # event.end_time = request.POST['end_time']
+        # event.end_date = request.POST['end_date']
+        # event.is_student=True
+        # is_student = request.POST['organizer']
+        # if(is_student == 1):
+        #     event.is_student=False
+        # event.postor = request.FILES['filename']
+        # event.registration_link = request.POST['registration']
 
 
-        place_name = request.POST['place']
-        # print(Place.objects.get(place_name=place_name))
-        event.event_place = Place.objects.get(place_name=place_name)
+        # place_name = request.POST['place']
+        # # print(Place.objects.get(place_name=place_name))
+        # event.event_place = Place.objects.get(place_name=place_name)
 
 
-        event.event_type = int(request.POST['level'])
-        event.save()
+        # event.event_type = int(request.POST['level'])
+        # event.save()
 
 
 
